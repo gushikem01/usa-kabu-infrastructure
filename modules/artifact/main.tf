b7620eab9a2cb0efcafc55dd192b850c93238afc
gushikem01 <gl_higaisha@yahoo.co.jp>
2024-04-30 00:11:51 +0900

    fix: artifact registry

--- modules/artifact/main.tf
+++ modules/artifact/main.tf
@@ -1,18 +1,6 @@
 resource "google_artifact_registry_repository" "default" {
-  repository_id = "usa-kabu-artifacts"
+  repository_id = "usa-kabu-jobs"
   location      = "us-central1"
   project       = var.project_id
   format        = "DOCKER"
-}
-
-resource "google_project_iam_member" "artifact_registry" {
-  project = var.project_id
-  role    = "roles/artifactregistry.reader"
-  member  = "serviceAccount:${google_service_account.jobs.email}"
-}
-
-resource "google_service_account" "jobs" {
-  account_id   = "usa-kabu-jobs"
-  display_name = "Jobs Service Account"
-  project      = var.project_id
 }
